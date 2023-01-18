9fcbe98f311c10c751ebe3f229d7f3d3f8fd8983
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-18 14:51:37 +0500

    member list with pagination for consultant

--- phase2.js
+++ phase2.js
@@ -1,6 +1,7 @@
 var db = firebase.firestore();
 
 $("#submit_university_program_button").on("click", function (e) {
+  let states_array = [];
   e.preventDefault();
   console.log("hello there");
 
@@ -9,10 +10,10 @@ $("#submit_university_program_button").on("click", function (e) {
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         statesdata = doc.data();
-
+        states_array = statesdata.name;
         // console.log(userfbdata.name + "data");
-        for (var counter = 0; counter < statesdata.length; counter++) {
-          console.log(statesdata[counter] + "coutner");
+        for (var counter = 0; counter < states_array.length; counter++) {
+          console.log(states_array[counter] + "coutner");
         }
 
         // $("#w-dropdown-list-0").append(
