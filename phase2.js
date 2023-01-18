3fc545cd159e17d4bf48b62df35c196b3daf9284
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-18 16:22:14 +0500

    commit logs

--- phase2.js
+++ phase2.js
@@ -17,10 +17,68 @@ $("#university_section").on("click", function (e) {
         );
       });
     });
+
+  //   $("#Type-name-of-University").autocomplete({
+  //     source: function (request, response) {
+  //       console.log(request.term);
+  //       var array_univeristy = [];
+
+  //       var struniveristy = request.term;
+  //       var universitykeywordsval = struniveristy.split(" ");
+
+  //       db.collection("University")
+  //         .where("schoolkeywords", "array-contains-any", universitykeywordsval)
+  //         .limit(15)
+  //         .get()
+  //         .then((querySnapshot) => {
+  //           console.log(querySnapshot.size);
+  //           if (querySnapshot.size > 0) {
+  //             querySnapshot.forEach((doc) => {
+  //               statedata = doc.data();
+  //               // console.log(doc.id, " => ", doc.data());
+  //               var school_item = { id: doc.id, value: doc.data().schoolname };
+
+  //               array_schools.push(school_item);
+  //             });
+  //           } else {
+  //             school_item = { value: "", label: "Not Found" };
+  //             array_schools.push(school_item);
+  //           }
+  //           console.log(array_schools);
+  //           response(array_schools);
+  //         })
+  //         .catch((error) => {
+  //           // The document probably doesn't exist.
+  //           console.error("No results found: ", error);
+  //         });
+  //     },
+  //     minLength: 2,
+  //     select: function (event, ui) {
+  //       if (ui.item.value == "Not Found") {
+  //         $(this).removeAttr("data-id");
+  //       } else {
+  //         $(this).attr("data-id", ui.item.id);
+  //         $("#selected-school-name").html(ui.item.value);
+  //         console.log(ui.item);
+  //       }
+  //     },
+  //     error: function (data) {
+  //       $('.input_addschool_field"').removeClass("ui-autocomplete-loading");
+  //     },
+  //   });
 });
 
 $("#submit_university_program_button").on("click", function (e) {
   let states_array = [];
   e.preventDefault();
   console.log("hello there");
+
+  db.collection("Univeristy")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        univeistydata = doc.data();
+        console.log(univeistydata);
+      });
+    });
 });
