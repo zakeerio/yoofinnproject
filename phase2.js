53307ec15fffb71c425cdac5ddff76f72f4551a3
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-18 16:26:13 +0500

    commit logs

--- phase2.js
+++ phase2.js
@@ -73,7 +73,7 @@ $("#submit_university_program_button").on("click", function (e) {
   e.preventDefault();
   console.log("hello there");
 
-  db.collection("States")
+  db.collection("Univeristy")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
