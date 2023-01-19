8e3370469ec0211eba5fa323b0331a0a1afe8b0a
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-19 15:38:22 +0500

    append university list

--- phase2.js
+++ phase2.js
@@ -225,7 +225,7 @@ $("#add_university_button").on("click", function (e) {
   univeristy_array.push(univeristy_object);
 
   let univeristy_div_to_append =
-    "<div class='UniversityOf'><a href='#' class='anchor-img w-inline-block'><img src='https://uploads-ssl.webflow.com/629a6c53c8ec9fdc6019d9f8/63c83b23bcdbf2331f6d0cc2_Vector%20(20).svg' loading='lazy' alt=''></a><div class='heading-14'>" +
+    "<div class='UniversityOf mt-4'><a href='#' class='anchor-img w-inline-block'><img src='https://uploads-ssl.webflow.com/629a6c53c8ec9fdc6019d9f8/63c83b23bcdbf2331f6d0cc2_Vector%20(20).svg' loading='lazy' alt=''></a><div class='heading-14'>" +
     univeristy_name +
     "</div><div class='tittle-flex'><div class='heading-12'>" +
     department_name +
