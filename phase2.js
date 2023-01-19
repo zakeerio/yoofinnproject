e01c5c0e23185db9a1ef3d03a459b22bd4d5f008
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-19 13:00:45 +0500

    function call to set the state value

--- phase2.js
+++ phase2.js
@@ -138,7 +138,9 @@ $("#university_section").on("click", function (e) {
 
         if (counter_append === 0) {
           $("#w-dropdown-list-0").html(
-            "<a href='#' class='w-dropdown-link' tabindex='0'>ALL</a><a href='#' class='w-dropdown-link' tabindex='0' onclick='setstatevalue()'>" +
+            "<a href='#' class='w-dropdown-link' tabindex='0'>ALL</a><a href='#' class='w-dropdown-link' tabindex='0' onclick='setstatevalue(" +
+              statesdata.name +
+              ")'>" +
               statesdata.name +
               "</a>"
           );
