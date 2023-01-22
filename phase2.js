dc24bbc8f954148617cb88f82cf7a3a71a953a50
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-22 23:27:30 +0500

    ticked p disabled krna hai

--- phase2.js
+++ phase2.js
@@ -247,8 +247,6 @@ $("#add_university_button").on("click", function (e) {
     "<div class='university_list' id='univeristy_" +
       univeristy_counter +
       "'><a href='#' class='anchor-img w-inline-block' onclick='closeuniversitypanel(" +
-      univeristy_name +
-      "_" +
       univeristy_counter +
       +")'><img src='https://uploads-ssl.webflow.com/629a6c53c8ec9fdc6019d9f8/63c83b23bcdbf2331f6d0cc2_Vector%20(20).svg' loading='lazy' alt=''></a><div class='heading-14'>" +
       univeristy_name +
