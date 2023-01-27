771f9711f7ea60ef528141c3fa38aab67344b1b9
Fatima Hassan <fatimahassan3232@gmail.com>
2023-01-27 15:01:09 +0500

    tick on the splash screen

--- phase2.js
+++ phase2.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
     });
   }
 
-  console.log(career_section_status);
+  console.log(career_section_status + "career_section_status");
   if (career_section_status === true) {
     $("#careers_section .dashboard-image").each(function () {
       var curSrc = $(this).attr("src");
