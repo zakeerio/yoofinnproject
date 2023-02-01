4b2db75b82a4d6995a5e84276e8c4982b1f1461c
Fatima Hassan <fatimahassan3232@gmail.com>
2023-02-01 17:04:37 +0500

    hide default career

--- phase2.js
+++ phase2.js
@@ -983,7 +983,7 @@ function get_the_data(career, salary, growth) {
         careerdata = doc.data();
         localStorage.setItem("career_Test", JSON.stringify(careerdata));
 
-        console.log(careerdata.ID + "careerdata");
+        console.log(doc.id + "careerdata");
         if (
           careerdata.title === career ||
           careerdata.salary === salary ||
