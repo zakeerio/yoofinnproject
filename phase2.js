7eeda0e8619886862b7e4564c47ecd77c5e85f51
Fatima Hassan <fatimahassan3232@gmail.com>
2023-03-30 10:10:36 +0500

    last_action_date

--- phase2.js
+++ phase2.js
@@ -1139,7 +1139,7 @@ function get_the_data(career, salary, growth) {
           console.log(index_of_career + "index_of_career");
           if (index_of_career > -1) {
           } else {
-            $("#career_parent_list").append(
+            $("#career_parent_list").html(
               "<div class='training flex'><div class='results'><div class='social-heading16 mb'><span class='light-sky-blue'>" +
                 half_name +
                 "</span>" +
