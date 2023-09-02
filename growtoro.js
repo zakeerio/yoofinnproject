b74d7aa5b6a3220d727673b988208b86acf0e710
Zahid Ali <=>
2023-09-03 03:23:37 +0500

    Updated growtoro.js for api call in stripe

--- growtoro.js
+++ growtoro.js
@@ -1,4 +1,4 @@
-$('.submit-btn').on('click', function() {
+$('.submit-checkout').on('click', function() {
     // TEST
     // var stripe = Stripe('pk_test_51IVH1EAbTE2jKd8otfWEVhotk6EAxOZVARanEwDPzZEE9THGdRCLfQv21JV79oqnIuCyFpVawhvsmU0m8F5v5Ri700imuCCbWV');
 
@@ -8,45 +8,11 @@ $('.submit-btn').on('click', function() {
 
     // $('.checkoutbutton').click(function(e){
     //     e.preventDefault();
-    var priceId = matchedValue; //'price_1LW2VTAbTE2jKd8oPI5rRbMp'; //$(this).attr('data-priceId');
+    var priceId = 'price_1LW2VTAbTE2jKd8oPI5rRbMp'; //$(this).attr('data-priceId');
     var planId = 'BASIC';
     var planqty = 1;
     var domainname = window.location.hostname;
-    console.log('Here');
-
-    var settings = {
-        "url": "https://api2.growtoro.com/api/charge.php",
-        // "url": 'https://stripe-api.test/api/charge.php',
-
-        "type": "POST",
-        "timeout": 0,
-        "headers": {
-            "Authorization": "key__8c0d8aa13bf726012e1314e8ad8b4175665b6777513accd79ccb7c63d19706a5",
-            "Content-Type": "application/json"
-        },
-        "data": JSON.stringify({
-            'couponcode' : coupon
-        })
-        
-    };
-
-
-    $.ajax(settings).done(function (response) {
-        if(response.id) {
-            calculatepromo(response);
-        } else {
-            $('div#notice').html(response.error)
-            $('div#notice').css({
-                "color": "red",
-                "text-align": "center"
-            });
-            $('.plan-price-right').html('$' + originalprice);
-            $('#totalamount, #subtotal').html('$' + originalprice); 
-            $('div#notice').show();
-            // alert(response.error);
-        }
-    });
-
+    
     var settings = {
         "url": "https://stripeapi.pixeltechnosol.com/growtorostripeapi/charge.php",
         "type": "POST",
