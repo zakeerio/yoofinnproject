7a50ca2c438b026a3fa5d7a81fbbaec3c4f48d15
Zahid Ali <=>
2023-11-18 01:46:12 +0500

    added avatar-ai file for firebase login authentication and stripe

---
+++ avatar-ai.js
@@ -0,0 +1,156 @@
+init
+
+$("#Login_button").on('click', function (e) {
+    e.preventDefault();
+
+    $("#error-message").html("");
+
+    var useremail = $("#email").val();
+    var userpassword = $("#password").val();
+
+    console.log(useremail);
+    console.log(userpassword);
+
+    if (useremail == "") {
+        $("#email").addClass('error');
+        // alert('Please enter an email address.');
+        errors += "<li>Please enter value in email field</li>";
+    }
+
+    if (userpassword.length < 8 || userpassword == "") {
+        $("#password").addClass('error');
+        errors += "<li>Please enter a password</li>";
+    }
+
+    //Sign In User with Email and Password
+    firebase.auth().signInWithEmailAndPassword(useremail, userpassword).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        $("#error-message").html(errorMessage);
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+
+})
+
+
+$('#Loginwithgoogle').on('click', function(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    toggleSignIn(provider);
+})
+
+
+/**
+ * Function called when clicking the Login/Logout button.
+ */
+function toggleSignIn(provider) {
+    $(".error-message").html("");
+    if (!firebase.auth().currentUser) {
+
+        // provider.addScope('user_birthday');
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            $('#quickstart-oauthtoken').text(token);
+
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            console.log(user);
+
+            // if (email) {
+            //     var userdata = writeNewPost(uid, displayName, email);
+            //     if (userdata) {
+            //         $(".registration").addClass('hide');
+            //         // window.location.href="/registration";
+            //     }
+            // }
+
+
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            console.log(errorCode+" "+errorMessage);
+            $(".error-message").html(errorMessage);
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            if (errorCode === 'auth/account-exists-with-different-credential') {
+                alert('You have already signed up with a different auth provider for that email.');
+                // If you are using multiple auth providers on your app you should handle linking
+                // the user's accounts here.
+            } else {
+                console.error(error);
+            }
+        });
+    } else {
+        firebase.auth().signOut();
+    }
+//   $('#Loginwithfacebook').attr('disabled', true);
+}
+
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+    // Listening for auth state changes.
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            localStorage.setItem('userdata', JSON.stringify(user));
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            console.log(email);
+            if (email) {
+                var userdata = writeNewPost(uid, displayName, email);
+                console.log(userdata);
+                if (userdata) {
+                    console.log("here");
+                    $(".registration").addClass('hide');
+                    window.location.href="/registration";
+                }
+            }
+            
+
+        } else {
+            localStorage.removeItem('userdata');
+            localStorage.removeItem('userfbdata');
+            // User is signed out.
+            console.log('user logged out!');
+            
+        }
+    });
+
+    // $('#Loginwithfacebook').on('click', function(){
+    //     var provider = new firebase.auth.FacebookAuthProvider();
+    //     toggleSignIn(provider);
+    // })
+
+    $('#Loginwithgoogle').on('click', function(){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        toggleSignIn(provider);
+    })
+}
+
+window.onload = function() {
+    initApp();
+};
+
